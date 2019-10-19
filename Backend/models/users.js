f8afd8592267c5a3bb5974baa76c0534f0e5ee4d
filmonabr <filmonabr@gmail.com>
2019-10-19 17:02:48 -0500

    schema for user created

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -1,4 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    usertype: {
+        type: String,
+        default: 'user'
+    }
+});
