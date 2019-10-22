e7e5cd515ba338a1189e72c1645ddb1330c60f5e
luwamsolomon <luwamsolomon11@gmail.com>
2019-10-22 11:02:02 -0500

    update service

--- client/src/app/products/service/product.service.ts
+++ client/src/app/products/service/product.service.ts
@@ -61,7 +61,9 @@ export class ProductService {
     return this.http.post('products/create', data).toPromise();
   }
 
-  
+  deleteProduct(id) {
+    return this.http.delete('products/' + id).toPromise();
+  }
 
   formateProduct(data, cartList) {
     const products = cloneDeep(data);
