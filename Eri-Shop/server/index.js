36fdd1fdab1396992c483cf28991b223046aa7a1
filmonabr <39414083+filmonabr@users.noreply.github.com>
2019-10-22 19:47:04 -0500

    Update index.js
    user registration and authentication modified

--- Eri-Shop/server/index.js
+++ Eri-Shop/server/index.js
@@ -29,9 +29,40 @@ app.use('/api/products', productRoute);
 app.use('/api/dictionary', dictionaryRoute);
 app.use('/api/cart', cartRoute);
 
-app.use('/', express.static(__dirname + '/dist/eCommerce'));
 
-app.use('*', express.static(__dirname + '/dist/eCommerce'));
+app.post('/register', (req, res)=>{
+    const userData = req.body; 
+    const user = new User(userData);
+
+    console.log(userData)
+    user.save((err, result)=>{
+        if(err)
+             console.log('Saving User Error')
+        res.status(200).send('Congrats');
+    })
+
+})
+
+app.post('/login', async (req, res)=>{
+    const loginData = req.body;
+
+    const user = await User.findOne({email: loginData.email});
+
+    if(!user)
+        return res.status(401).send({message:"Email or Password Invalid !"})
+    
+    bcrypt.compare(loginData.pwd, user.pwd, (err, isMatch)=>{
+        if(!isMatch){
+            return res.status(401).send({message:"Email or Password Invalid by BCRYPT !"})
+        }
+        
+        let payload = {}
+
+        let token = jwt.encode(payload, '123');
+         res.status(200).send({token}).send('You are in')
+      //  res.send('You are in')
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server listen from ${PORT}.....`);
